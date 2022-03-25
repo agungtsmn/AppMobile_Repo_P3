a2a2ba52b1c217591ca75d4c6b1c625074269a00
agungtsmn <0812agung@gmail.com>
2022-03-25 15:38:43 +0700

    Percobaan Inheritance - Overriding Property’s Getter/Setter method

---
+++ src/main/kotlin/id/ac/polbeng/Inheritance/InheritanceE.kt
@@ -0,0 +1,20 @@
+package id.ac.polbeng.Inheritance
+
+open class NewPerson {
+    open var age: Int = 1
+}
+
+class CheckedPerson: NewPerson() {
+    override var age: Int = 1
+        set(value) {
+            field = if(value > 0) value else throw IllegalArgumentException("Age can not be negative")
+        }
+}
+
+fun main() {
+    val person = NewPerson()
+    person.age = -5
+
+    val checkedPerson = CheckedPerson()
+    checkedPerson.age = -5
+}
